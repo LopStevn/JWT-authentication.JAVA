7b0fb358f96c5f8a835ddb239439d4604b1e0e33
LopStevn <stevenlpz03@gmail.com>
2023-08-12 18:04:41 -0600

    Entidad Token Response

--- JWT-COOKIE.EntidadesDeNegocio/src/jwt/cookie/TokenResponse.java
+++ JWT-COOKIE.EntidadesDeNegocio/src/jwt/cookie/TokenResponse.java
@@ -10,4 +10,22 @@ package jwt.cookie;
  */
 public class TokenResponse {
     
+    private String token;
+
+    public TokenResponse() {
+    }
+
+    public TokenResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+    
+    
 }
